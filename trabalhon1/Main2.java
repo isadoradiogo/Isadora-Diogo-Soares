39075070b9a29dd225673185cc4fac0d1f259149
isadoradiogo <79872780+isadoradiogo@users.noreply.github.com>
2021-03-18 16:51:06 -0300

    Add files via upload

---
+++ trabalhon1/Main2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhon1;
+
+/**
+ *
+ * @author 55519
+ */
+public class Main2 {
+    
+}
---
+++ trabalhon1/TrabalhoN1.java
@@ -0,0 +1,22 @@
+
+package trabalhon1;
+import java.util.Scanner;
+import prduto2;
+
+public class TrabalhoN1 {
+
+    
+    public static void main(String[] args) {
+        
+    }
+     Scanner ler = new Scanner (System.in);
+        produto = new produto();
+                
+        System.out.println("Digite o nome do programa");
+        produto.nome = in.nextLine();
+        System.out.println("Digite o valor do produto");
+        produto.valor = in.nestDouble();
+        
+        System.out.println(produto);
+    }
+  
